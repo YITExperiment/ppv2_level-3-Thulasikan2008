99d235016ceb776edb291ec67e4d27c7aa4c63cd
Thulasikan2008 <134524632+Thulasikan2008@users.noreply.github.com>
2023-05-27 20:46:17 +0530

    Add files via upload

---
+++ myfirstprogram.py
@@ -0,0 +1,16 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========== RESTART: C:/Users/PC/OneDrive/Desktop/screen shot/home1.py ==========
+Hello, World!
+What is your name? v.Thulasikan
+Hello  v.Thulasikan
+>>> age=16
+>>> print(age)
+16
+>>> weight=51
+>>> print(weight)
+51
+>>> height=178
+>>> print(height)
+178

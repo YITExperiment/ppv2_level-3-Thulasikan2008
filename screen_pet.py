3ffa30f210b9bd524785094a0b8c7cf409e3d050
Thulasikan2008 <134524632+Thulasikan2008@users.noreply.github.com>
2023-05-27 20:48:35 +0530

    Add files via upload

---
+++ screen_pet.py
@@ -0,0 +1,14 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> screen_pet.
+SyntaxError: incomplete input
+>>> screen_pet.py
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    screen_pet.py
+NameError: name 'screen_pet' is not defined
+>>> screen_pet
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    screen_pet
+NameError: name 'screen_pet' is not defined
